90ff70d484a143fa2b1e91dc83c6b4c44cf54e16
Sergey Kudashev <sergey.m.kudashev@gmail.com>
2025-06-11 16:06:59 +0300

    finish

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,17 @@ import Form from "./components/Form";
 import FilterButton from "./components/FilterButton";
 import Todo from "./components/Todo";
 
+const FILTER_MAP = {
+  All: () => true,
+  Active: (task) => !task.completed,
+  Completed: (task) => task.completed,
+};
+
+const FILTER_NAMES = Object.keys(FILTER_MAP);
+
 function App(props) {
   const [tasks, setTasks] = useState(props.tasks);
+  const [filter, setFilter] = useState("All");
   function addTask(name) {
     const newTask = { id: `todo-${nanoid()}`, name, completed: false };
     setTasks([...tasks, newTask]);
@@ -31,19 +40,21 @@ function App(props) {
   }
 
   function editTask(id, newName) {
-  const editedTaskList = tasks.map((task) => {
-    // if this task has the same ID as the edited task
-    if (id === task.id) {
-      // Copy the task and update its name
-      return { ...task, name: newName };
-    }
-    // Return the original task if it's not the edited task
-    return task;
-  });
-  setTasks(editedTaskList);
-}
+    const editedTaskList = tasks.map((task) => {
+      // if this task has the same ID as the edited task
+      if (id === task.id) {
+        // Copy the task and update its name
+        return { ...task, name: newName };
+      }
+      // Return the original task if it's not the edited task
+      return task;
+    });
+    setTasks(editedTaskList);
+  }
 
-  const taskList = tasks?.map((task) => (
+  const taskList = tasks
+  .filter(FILTER_MAP[filter])
+  .map((task) => (
     <Todo
       id={task.id}
       name={task.name}
@@ -54,17 +65,23 @@ function App(props) {
       editTask={editTask}
     />
   ));
+
+  const filterList = FILTER_NAMES.map((name) => (
+  <FilterButton
+    key={name}
+    name={name}
+    isPressed={name === filter}
+    setFilter={setFilter}
+  />
+));
+
   const tasksNoun = taskList.length !== 1 ? "tasks" : "task";
   const headingText = `${taskList.length} ${tasksNoun} remaining`;
   return (
     <div className="todoapp stack-large">
       <h1>TodoMatic</h1>
       <Form addTask={addTask} />
-      <div className="filters btn-group stack-exception">
-        <FilterButton />
-        <FilterButton />
-        <FilterButton />
-      </div>
+      <div className="filters btn-group stack-exception">{filterList}</div>
       <h2 id="list-heading">{headingText}</h2>
       <ul
         role="list"
